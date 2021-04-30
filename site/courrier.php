28cf0d30729ec5b4971638fbae650f3a29bac766
Patrick ANNOOT <patrickannoot@orange.fr>
2021-04-30 09:45:29 +0200

    ajout de courrier.php à développer

---
+++ site/courrier.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Courrier</title>
+</head>
+<body>
+    <p>
+        Bonjour <?php echo $_POST['name']; ?> !
+    </p>
+    <p><a href="index.html">Retour à l'accueil</a></p>
+    
+</body>
+</html>
