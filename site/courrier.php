db16a9884519052027a0027c3a0c9efb17515780
Patrick ANNOOT <patrickannoot@orange.fr>
2021-05-18 16:15:00 +0200

    travail sur formulaire et courrier

--- site/courrier.php
+++ site/courrier.php
@@ -8,9 +8,20 @@
 </head>
 <body>
     <p>
-        Bonjour <?php echo $_POST['name']; ?> !
+        Bonjour <?php echo $_POST['prenom']; ?> !
     </p>
     <p><a href="index.html">Retour à l'accueil</a></p>
+
+
     
+    <?php
+        echo('Prénom : '.$_POST["prenom"].'<br>');
+        echo('Nom : '.$_POST["name"].'<br>');
+        echo('Email : '.$_POST["email"].'<br>'); 
+        echo ('Téléphone : '.$_POST["telephone"].'<br>');
+        echo('Objet : '.$_POST["subject"].'<br>');
+        echo('Message : '.$_POST["message"]);
+                                
+    ?>
 </body>
 </html>
